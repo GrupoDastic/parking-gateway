8fff956d984828ef42b2a7a219d989874b124107
Mauricio Saldarriaga Morales <hugosmm2000@gmail.com>
2026-05-12 08:50:50 -0500

    add privacy policy page and configure static resource locations

---
+++ src/test/java/io/github/imecuadorian/gateway/ParkingGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.imecuadorian.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
